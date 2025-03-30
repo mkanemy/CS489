9a9d0410d72713d72b68e72f1e1e2efd6c40d6c3
Linus Zhang <l625zhan@uwaterloo.ca>
2025-03-30 00:35:57 -0400

    make sure plain text secret up to 256 chars can be accepted after encryption

--- webapi/main.py
+++ webapi/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5173",
+        "http://localhost:5173",
         "https://lockedin.quest",
     ],
     allow_methods=["*"],
